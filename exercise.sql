6676a5d4e6b08ede25fca7dd2eb241c14765b4d7
fedegot <90008948+fedegot@users.noreply.github.com>
2022-01-17 21:33:56 +0000

    Update exercise.sql

--- exercise.sql
+++ exercise.sql
@@ -115,6 +115,124 @@ WHERE Apprendice = '1'
 -- <>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@
 -- <>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@<>@
 
+
+SQL
+
+Activities 1
+
+#1 Just let students investigate the data
+
+#2
+SELECT * FROM current_job_detail_csv WHERE job_title = "Tech Lead";
+
+#3
+SELECT * FROM employee_detail_csv WHERE gender = "F";
+
+#4
+SELECT * FROM employee_detail_csv WHERE name LIKE ("S%");
+
+#5
+SELECT * FROM jobs_history_csv WHERE job_title LIKE ("%Developer");
+
+#6
+SELECT laptop_id FROM laptop_detail_csv WHERE os = "Ubuntu";
+
+#7
+SELECT * FROM employee_detail_csvWHERE name LIKE ("S%") OR name LIKE ("A%");
+
+Activities 1
+
+#1
+CREATE TABLE great_names (employee_id int PRIMARY KEY, name varchar(64));
+
+#2
+INSERT INTO great_names
+	SELECT employee_id, name FROM employee_detail_csv
+WHERE employee_id IN (1001, 1002, 1003, 1004, 1005);
+
+#3
+DELETE FROM great_names WHERE employee_id = 1005;
+
+#4
+ALTER TABLE great_names ADD COLUMN great_name_ind VARCHAR(1);
+
+#5
+UPDATE great_names SET great_name_ind = "Y";
+
+#6
+UPDATE great_names SET great_name_ind = "N" WHERE employee_id = 1001;
+
+Activities 3
+
+#1 
+
+SELECT * FROM laptop_detail_csv ldc 
+INNER JOIN current_job_detail_csv cjdc 
+ON ldc.laptop_id = cjdc.laptop_id;
+
+#2  SELECT * FROM laptop_detail_csv ldc 
+INNER JOIN current_job_detail_csv cjdc 
+ON ldc.laptop_id = cjdc.laptop_id 
+WHERE ldc.os = "Mac"; 
+#3
+ SELECT * FROM current_job_detail_csv cjdc 
+INNER JOIN jobs_history_csv jhc 
+ON cjdc.employee_id = jhc.employee_id
+WHERE jhc.job_title = "Apprentice Developer" AND cjdc.job_title = "Developer"; 
+SELECT * FROM jobs_history jh
+INNER JOIN jobs_history jh2
+INNER JOIN employee_detail ed
+ON jh.employee_id = jh2.employee_id
+AND jh.Employee_ID = ed.Employee_ID
+WHERE jh.job_title = "Apprentice Developer" AND jh2.job_title = "Developer";
+
+#4
+ SELECT * FROM current_job_detail_csv cjdc 
+INNER JOIN jobs_history_csv jhc 
+ON cjdc.employee_id = jhc.employee_id
+WHERE jhc.job_title not LIKE ("%Developer") AND cjdc.job_title LIKE ("%Developer"); 
+#5
+ SELECT * FROM current_job_detail_csv cjdc 
+INNER JOIN jobs_history_csv jhc 
+ON cjdc.employee_id = jhc.employee_id
+WHERE jhc.job_title != cjdc.job_title;
+
+#6
+
+SELECT DISTINCT cjd.employee_id, cjd.job_title FROM master23.current_job_detail cjd 
+INNER JOIN master23.jobs_history jh 
+ON cjd.employee_id = jh.employee_id
+WHERE jh.job_title != cjd.job_title;
+
+Activities 4
+
+#1
+SELECT job_title, MAX(salary) FROM current_job_detail_csv cjdc
+GROUP BY job_title;
+
+#2
+
+SELECT job_title, os, COUNT(os) FROM current_job_detail_csv cjdc
+INNER JOIN laptop_detail_csv ldc
+ON cjdc.laptop_id  = ldc.laptop_id 
+GROUP BY job_title, os;
+
+#3
+
+SELECT cjd.job_title, AVG(salary) FROM current_job_detail cjd 
+INNER JOIN jobs_history jh 
+ON cjd.employee_id = jh.employee_id 
+WHERE jh.job_title = 'Apprentice Developer'
+GROUP BY job_title;
+
+#4
+
+SELECT name
+FROM employee_detail ed
+INNER JOIN current_job_detail cjd 
+ON cjd.employee_id = ed.Employee_ID 
+ORDER BY cjd.salary DESC 
+LIMIT 1;
  		
 
 
